bf5dc08878b35362b6d6ea617feaa8af533b23aa
Adam Raynaut <snx123123@gmail.com>
2026-07-25 09:27:57 +0200

    zdsq

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -7,10 +7,17 @@ export default {
   async execute(client: any) {
     client.user.setPresence({
       status: 'online',
-      activities: [{ name: 'discord.gg/atlasrpfr', type: ActivityType.Watching }]
+      activities: [{
+        name: 'discord.gg/atlasrpfr',
+        type: ActivityType.Streaming,
+        url: 'https://www.twitch.tv/atlasrp_officiel' // Obligatoire pour le point violet
+      }]
     });
+
     console.log(`ATLAS BOT connecté : ${client.user.tag}`);
+
     try { await registerCommands(client); }
     catch (error) { console.error('Échec de l’enregistrement des slash-commandes', error); }
   }
 };
+
